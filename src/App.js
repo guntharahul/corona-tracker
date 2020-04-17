09e43057d8af821e6c472cfcbdad04720d6806e3
R@HUL <31180528+guntharahul@users.noreply.github.com>
2020-04-17 20:52:36 +0530

    Home page dashboard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { Chart, Cards, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+class App extends Component {
+  state = {
+    data: {},
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({
+      data: fetchedData,
+    });
+    console.log(this.state.data);
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data}></Cards>
+        {/* <CountryPicker></CountryPicker>
+        <Chart></Chart> */}
+      </div>
+    );
+  }
 }
 
 export default App;
